e504af3914fed170ad1d27735c195237334e85af
Waleed Lugod <lugodwaleed@gmail.com>
2025-11-19 15:45:17 +0800

    scan fixed tokens

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import scanner
 
-scanner.setup("samples/sample_input_1.txt")
-scanner.gettoken()
+filename = "test_input.txt"
+output_file = open(filename.replace("input", "output_scan_test"), "w")
+scanner.setup(filename)
+while True:
+    out = scanner.gettoken()
+    output_file.write(f"{out['token']:<17}{out['lexeme']}\n")
+    if out["token"] == "EndOfFile":
+        break
--- scanner.py
+++ scanner.py
@@ -1,10 +1,85 @@
+from enum import Enum
+
 idx = 0
-file = ""
+input_file = ""
+
+fixed_tokens = {
+    ":=": "Assign",
+    ";": "Semicolon",
+    ":": "Colon",
+    ",": "Comma",
+    "(": "LeftParen",
+    ")": "RightParen",
+    "+": "Plus",
+    "-": "Minus",
+    "*": "Multiply",
+    "/": "Divide",
+    "**": "Raise",
+    "<": "LessThan",
+    "=": "Equal",
+    ">": "GreaterThan",
+    "<=": "LTEqual",
+    ">=": "GTEqual",
+    "!=": "NotEqual",
+    "PRINT": "Print",
+    "IF": "If",
+    "ELSE": "Else",
+    "ENDIF": "EndIf",
+    "SQRT": "Sqrt",
+    "AND": "And",
+    "OR": "Or",
+    "NOT": "Not",
+}
+
+
+class States(Enum):
+    NONE = 0
+    Error = 1
+    Identifier = 2
+    Number = 3
+    String = 4
+    Comment = 5
+
 
 def setup(filename):
-    file = open(filename).read()
+    global input_file
+    global idx
+    input_file = open(filename).read()
     idx = 0
 
+
 def gettoken():
+    global idx
+    token = ""
     lexeme = ""
-    return lexeme
+    build = ""
+    state = States.NONE
+
+    if idx < len(input_file):
+        c = input_file[idx]
+        while c == " " or c == "\t" or c == "\n":
+            idx += 1
+            c = input_file[idx]
+        lexeme = c
+        if c == ":" or c == "<" or c == ">" or c == "!":
+            if input_file[idx + 1] == "=":
+                lexeme += input_file[idx + 1]
+                idx += 1
+            elif c == "!":
+                state = States.Error
+        elif c == "*":
+            if input_file[idx + 1] == "*":
+                lexeme += input_file[idx + 1]
+                idx += 1
+        elif lexeme not in fixed_tokens:
+            state = States.Error
+
+        if state == States.Error:
+            token = States.Error.name
+        elif state == States.NONE:
+            token = fixed_tokens[lexeme]
+    else:
+        token = "EndOfFile"
+
+    idx += 1
+    return {"token": token, "lexeme": lexeme}
